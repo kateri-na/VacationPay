ffecffdafcde624e6e2d85ebfd8421a78291273a
katerina <katerina_panina1@mail.ru>
2024-09-19 23:07:20 +0300

    improve calculation method and inject service

--- src/main/java/com/Panina/VacationPay/controllers/VacationPayController.java
+++ src/main/java/com/Panina/VacationPay/controllers/VacationPayController.java
@@ -1,14 +1,26 @@
 package com.Panina.VacationPay.controllers;
 
+import com.Panina.VacationPay.services.VacationPayService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @RequestMapping("/calculate")
 @Controller
 public class VacationPayController {
+    @Autowired
+    private VacationPayService service;
+
     @GetMapping
-    public String calculation(){
+    public String calculation(@RequestParam(value = "yearAverageSalary", required = false) Double yearAverageSalary,
+                              @RequestParam(value = "numberOfVacationDays", required = false) Integer numberOfVacationDays,
+                              Model model){
+        if (yearAverageSalary != null && numberOfVacationDays != null)
+            model.addAttribute("vacationPay",
+                    service.calculateVacationPay(yearAverageSalary, numberOfVacationDays));
         return "calculation";
     }
 }
