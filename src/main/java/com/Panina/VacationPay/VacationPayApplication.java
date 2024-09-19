2f89ee21c21ca5f5787d4ee62692decd2d0a3c13
katerina <katerina_panina1@mail.ru>
2024-09-19 21:50:41 +0300

    Initial commit

---
+++ src/main/java/com/Panina/VacationPay/VacationPayApplication.java
@@ -0,0 +1,13 @@
+package com.Panina.VacationPay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Panina/VacationPay/VacationPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Panina.VacationPay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
