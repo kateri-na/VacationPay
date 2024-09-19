9bf6abe74605ddabec300601cd701190756f7d1a
katerina <katerina_panina1@mail.ru>
2024-09-19 23:19:24 +0300

    add service calculation method

--- src/main/java/com/Panina/VacationPay/services/VacationPayService.java
+++ src/main/java/com/Panina/VacationPay/services/VacationPayService.java
@@ -4,4 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class VacationPayService {
+    private static final double DAYS_IN_MONTH = 29.3;
+
+    public double calculateVacationPay(double yearAverageSalary, int numberOfVacationDays){
+        double dailyAverageSalary = yearAverageSalary / DAYS_IN_MONTH;
+        return Math.round(dailyAverageSalary * numberOfVacationDays);
+    }
 }
