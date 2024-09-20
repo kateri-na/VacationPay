12a8ff3cbcdd5e167dcef723312ac58980994151
katerina <katerina_panina1@mail.ru>
2024-09-20 16:57:13 +0300

    add calculation with vacation days

--- src/main/java/com/Panina/VacationPay/controllers/VacationPayController.java
+++ src/main/java/com/Panina/VacationPay/controllers/VacationPayController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
 
+import java.time.LocalDate;
+
 @RequestMapping("/calculate")
 @Controller
 public class VacationPayController {
@@ -17,10 +19,18 @@ public class VacationPayController {
     @GetMapping
     public String calculation(@RequestParam(value = "yearAverageSalary", required = false) Double yearAverageSalary,
                               @RequestParam(value = "numberOfVacationDays", required = false) Integer numberOfVacationDays,
+                              @RequestParam(value = "startOfVacation", required = false) LocalDate startOfVacation,
+                              @RequestParam(value = "endOfVacation", required = false) LocalDate endOfVacation,
                               Model model){
-        if (yearAverageSalary != null && numberOfVacationDays != null)
-            model.addAttribute("vacationPay",
+        if (yearAverageSalary != null && numberOfVacationDays != null) {
+            if(startOfVacation != null && endOfVacation != null)
+                model.addAttribute("vacationPay", service.calculateVacationPay(yearAverageSalary,
+                        numberOfVacationDays, startOfVacation, endOfVacation));
+            else
+                model.addAttribute("vacationPay",
                     service.calculateVacationPay(yearAverageSalary, numberOfVacationDays));
+
+        }
         return "calculation";
     }
 }
--- src/main/java/com/Panina/VacationPay/services/VacationPayService.java
+++ src/main/java/com/Panina/VacationPay/services/VacationPayService.java
@@ -2,6 +2,10 @@ package com.Panina.VacationPay.services;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 public class VacationPayService {
     private static final double DAYS_IN_MONTH = 29.3;
@@ -9,5 +13,38 @@ public class VacationPayService {
     public double calculateVacationPay(double yearAverageSalary, int numberOfVacationDays){
         double dailyAverageSalary = yearAverageSalary / DAYS_IN_MONTH;
         return Math.round(dailyAverageSalary * numberOfVacationDays);
+    }
+    public double calculateVacationPay(double yearAverageSalary, int numberOfVacationDays,
+                                       LocalDate startOfVacation, LocalDate endOfVacation){
+        int year = endOfVacation.getYear();
+        Set<LocalDate> holidays = createHolidays(year);
+        for(LocalDate holiday:holidays){
+            if(holiday.isEqual(startOfVacation) || holiday.isEqual(endOfVacation))
+                --numberOfVacationDays;
+            else if (holiday.isAfter(startOfVacation) && holiday.isBefore(endOfVacation)) {
+                --numberOfVacationDays;
+
+            }
+        }
+        return calculateVacationPay(yearAverageSalary, numberOfVacationDays);
+    }
+
+    private Set<LocalDate> createHolidays(int year){
+        Set<LocalDate> holidays = new HashSet<LocalDate>();
+        holidays.add(LocalDate.parse(year + "-01-01"));
+        holidays.add(LocalDate.parse(year + "-01-02"));
+        holidays.add(LocalDate.parse(year + "-01-03"));
+        holidays.add(LocalDate.parse(year + "-01-04"));
+        holidays.add(LocalDate.parse(year + "-01-05"));
+        holidays.add(LocalDate.parse(year + "-01-06"));
+        holidays.add(LocalDate.parse(year + "-01-07"));
+        holidays.add(LocalDate.parse(year + "-01-08"));
+        holidays.add(LocalDate.parse(year + "-02-23"));
+        holidays.add(LocalDate.parse(year + "-03-08"));
+        holidays.add(LocalDate.parse(year + "-05-01"));
+        holidays.add(LocalDate.parse(year + "-05-09"));
+        holidays.add(LocalDate.parse(year + "-06-12"));
+        holidays.add(LocalDate.parse(year + "-11-14"));
+        return holidays;
     }
 }
