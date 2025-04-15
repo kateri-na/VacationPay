0d7bf6944cace7ebbe5ddbdf295d84bd29cbe5c2
ЕкатеринаПанина <katerina_panina1@mail.ru>
2025-04-15 21:43:33 +0300

    Modified controller to rest

--- src/main/java/com/Panina/VacationPay/controllers/VacationPayController.java
+++ src/main/java/com/Panina/VacationPay/controllers/VacationPayController.java
@@ -7,30 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
 @RequestMapping("/calculate")
-@Controller
+@RestController
 public class VacationPayController {
     @Autowired
     private VacationPayService service;
 
     @GetMapping
-    public String calculation(@RequestParam(value = "yearAverageSalary", required = false) Double yearAverageSalary,
+    public Double calculation(@RequestParam(value = "yearAverageSalary", required = false) Double yearAverageSalary,
                               @RequestParam(value = "numberOfVacationDays", required = false) Integer numberOfVacationDays,
                               @RequestParam(value = "startOfVacation", required = false) LocalDate startOfVacation,
                               @RequestParam(value = "endOfVacation", required = false) LocalDate endOfVacation,
                               Model model){
         if (yearAverageSalary != null && numberOfVacationDays != null) {
             if(startOfVacation != null && endOfVacation != null)
-                model.addAttribute("vacationPay", service.calculateVacationPay(yearAverageSalary,
-                        numberOfVacationDays, startOfVacation, endOfVacation));
+                return service.calculateVacationPay(yearAverageSalary, numberOfVacationDays, startOfVacation, endOfVacation);
             else
-                model.addAttribute("vacationPay",
-                    service.calculateVacationPay(yearAverageSalary, numberOfVacationDays));
-
+                return service.calculateVacationPay(yearAverageSalary, numberOfVacationDays);
         }
-        return "calculation";
+        return null;
     }
 }
